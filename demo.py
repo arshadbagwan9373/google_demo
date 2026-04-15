20a327a7b0159cce83396f64f8b220349674e1ed
Arshad Bagwan <arshadbagwan9373@gmail.com>
2026-04-15 12:49:03 +0530

    .

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
-print("-------------------- // -------------------------------\n")
+print("-------------------- // -------------------------------")
+print("----------- GOOGLE CLOUD ENGINE PRACTICAL ---------------")
+print("---------------------------------------------------------\n")
 print("This Code is Written in python and stored at GitHub ")
 print("Hello From GitHub.............")
 name = input("Enter your Name : ")
 print("Hello "+name + ".... Welcome To Cloud Engine")
 
 
-for(i=0;i<5;i++):
-  for(j=0;j<=i;j++):
+for i in range(0,15):
+  for j in range(0,i+1):
       print("*",end="")
   print("")
 print("---------------------- // -------------------------------")
